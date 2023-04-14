1325884ddc22694248def6f110b05ea4a32513f8
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-14 20:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.promises = [
-	new Promise((resolve) => setTimeout()=> (resolve(1) Math.random()*5000+1000));
-	new Promise((resolve) => setTimeout() => (resolve(2) Math.random() *5000 + 1000));
-	new Promise((resolve) => setTimeout() => (resolve(3) Math.random() *5000 + 1000));
-	new Promise((resolve) => setTimeout() => (resolve(4) Math.random() *5000 + 1000));
-	new Promise((resolve) => setTimeout() => (resolve(5) Math.random() *5000 + 1000));
+	const promise 1 = new Promise((resolve) => setTimeout()=> (resolve(1) Math.random()*5000+1000));
+	const promise 2 = new Promise((resolve) => setTimeout() => (resolve(2) Math.random() *5000 + 1000));
+	const promise 3 = new Promise((resolve) => setTimeout() => (resolve(3) Math.random() *5000 + 1000));
+	const promise 4 = new Promise((resolve) => setTimeout() => (resolve(4) Math.random() *5000 + 1000));
+	const promise 5 = new Promise((resolve) => setTimeout() => (resolve(5) Math.random() *5000 + 1000));
 ];
 
 promise.any(promises)
